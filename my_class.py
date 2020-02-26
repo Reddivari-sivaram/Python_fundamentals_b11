0f05559b3bb176b22bc5458f1c9a5d500fff0e7b
sivaram-sql <59366481+sivaram-sql@users.noreply.github.com>
2020-02-26 23:19:38 +0530

    Add files via upload

---
+++ my_class.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+class my_class:
+    """A simple class which is about my self in collge"""
+    def __init__(self,name,rollnum):
+        """intializing name and rollnum attributes"""
+        self.name=name
+        self.rollnum=rollnum
+    def my_self(self):
+        """Printing about my self"""
+        print(f"my name is {self.name.title()}")
+        print(f"my rollnum is {self.rollnum}")
+        
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
